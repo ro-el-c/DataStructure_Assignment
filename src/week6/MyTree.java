3a80f86c7ad5d4fbd62e3607508742dd6807d354
ro-el-c <chung3286@naver.com>
2022-04-06 16:00:52 +0900

    add week6 MyTree

---
+++ src/week6/MyTree.java
@@ -0,0 +1,76 @@
+package week6;
+
+import java.util.ArrayList;
+
+public class MyTree {
+    private MyNode root;
+    private int treeSize;
+
+    MyTree(){
+        this.root = null;
+        this.treeSize = 0;
+    }
+    MyTree(Object obj){
+        this.root = new MyNode(obj);
+        this.treeSize = 1;
+        this.root.setChildren(new ArrayList());
+    }
+
+    public int size(){
+        return this.treeSize;
+    }
+
+    public MyNode root(){
+        return this.root;
+    }
+
+    public ArrayList children(MyNode v){
+        return v.children();
+    }
+
+    public boolean isExternal(MyNode v){
+        return v.children().size() == 0;
+    }
+
+    public MyNode addroot(Object obj){
+        this.root = new MyNode(obj);
+        this.treeSize++;
+        this.root.setChildren(new ArrayList());
+        return this.root;
+    }
+
+    public MyNode addNode(Object obj){
+        MyNode newNode = new MyNode(obj);
+        newNode.setChildren(new ArrayList());
+        this.root.children().add(newNode);
+        this.treeSize++;
+        return newNode;
+    }
+
+    public MyNode addChild(MyNode v, Object obj){
+        MyNode newNode = new MyNode(obj);
+        newNode.setChildren(new ArrayList());
+        v.children().add(newNode);
+        this.treeSize++;
+        return newNode;
+    }
+
+    public MyNode addChild(MyNode v, int i, Object obj){
+        MyNode newNode = new MyNode(obj);
+        newNode.setChildren(new ArrayList());
+        v.children().add(i, newNode);
+        this.treeSize++;
+        return newNode;
+    }
+
+    public MyNode setChild(MyNode v, int i, Object obj){
+        MyNode newNode = new MyNode(obj);
+        newNode.setChildren(new ArrayList());
+        v.children().set(i, newNode);
+        return newNode;
+    }
+
+    public MyNode removeChild(MyNode v, int i){
+        return (MyNode)v.children().remove(i);
+    }
+}
