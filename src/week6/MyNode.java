605256938e0ff9d7505b6f9ed6c5330fba92965b
ro-el-c <chung3286@naver.com>
2022-04-06 15:21:14 +0900

    add week6 MyNode

---
+++ src/week6/MyNode.java
@@ -0,0 +1,44 @@
+package week6;
+
+import java.util.ArrayList;
+
+public class MyNode {
+    private Object element;
+    private MyNode parent;
+    private ArrayList children;
+
+    MyNode(){
+        this.element = null;
+        this.parent = null;
+        this.children = null;
+    }
+    MyNode(Object obj){
+        this.element = obj;
+        this.parent = null;
+        this.children = null;
+    }
+
+    public Object element(){
+        return this.element;
+    }
+
+    public MyNode parent(){
+        return this.parent;
+    }
+
+    public ArrayList children(){
+        return this.children;
+    }
+
+    public void setElement(Object obj){
+        this.element = obj;
+    }
+
+    public void setParent(MyNode par){
+        this.parent = par;
+    }
+
+    public void setChildren(ArrayList child) {
+        this.children = child;
+    }
+}
