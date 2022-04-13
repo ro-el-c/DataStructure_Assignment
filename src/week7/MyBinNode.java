97507c2feeebaa97f75110f4a5dc444eac67e4d7
ro-el-c <chung3286@naver.com>
2022-04-13 21:39:07 +0900

    modify week7 MyBinNode

--- src/week7/MyBinNode.java
+++ src/week7/MyBinNode.java
@@ -1,28 +1,22 @@
 package week7;
 
-import week6.MyNode;
-
 public class MyBinNode extends MyNode {
+    // constructors 생성자
     MyBinNode(){
         super();
     }
-    MyBinNode(Object obj){
-        super(obj);
+    MyBinNode(Object e) {
+        super(e);
     }
 
     public MyBinNode left(){
-        return (MyBinNode)super.children().get(0);
+        return (MyBinNode) super.children().get(0);
     }
-
     public MyBinNode right(){
-        return (MyBinNode)super.children().get(1);
+        return (MyBinNode) super.children().get(1);
     }
-
     public void setLeft(MyBinNode v){
         super.children().set(0, v);
     }
-
-    public void setRight(MyBinNode v){
-        super.children().set(1, v);
-    }
+    public void setRight(MyBinNode v){ super.children().set(1, v); }
 }
